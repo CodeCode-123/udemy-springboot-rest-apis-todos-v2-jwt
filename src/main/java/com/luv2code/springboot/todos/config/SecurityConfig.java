b379d0a95864fedacbfb7e5aec92f870ac3560b7
Shen Tang <stang311@gatech.edu>
2026-03-09 13:48:20 -0400

    Added RegisterRequest DTO, Authentication Service, and Controller classes to generate an authentication register API HTTP method

--- src/main/java/com/luv2code/springboot/todos/config/SecurityConfig.java
+++ src/main/java/com/luv2code/springboot/todos/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**",
+                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**",
                                 "/swagger-resources/**", "/webjars/**", "/docs").permitAll());
         // disable using CSRF token when using JWT token
         http.csrf(AbstractHttpConfigurer::disable);
