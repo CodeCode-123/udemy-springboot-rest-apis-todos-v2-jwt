645ad7695d3162dbec5a8352b91e98e21f5739ba
Shen Tang <stang311@gatech.edu>
2026-03-09 22:26:24 -0400

    Added password update API to the controller class, added the related method to the service, and the password update request DTO

--- src/main/java/com/luv2code/springboot/todos/service/UserService.java
+++ src/main/java/com/luv2code/springboot/todos/service/UserService.java
@@ -1,7 +1,9 @@
 package com.luv2code.springboot.todos.service;
+import com.luv2code.springboot.todos.request.PasswordUpdateRequest;
 import com.luv2code.springboot.todos.response.UserResponse;
 
 public interface UserService {
     UserResponse getUserInfo();
     void deleteUser();
+    void updatePassword(PasswordUpdateRequest passwordUpdateRequest);
 }
--- src/main/java/com/luv2code/springboot/todos/service/UserServiceImpl.java
+++ src/main/java/com/luv2code/springboot/todos/service/UserServiceImpl.java
@@ -3,11 +3,14 @@ package com.luv2code.springboot.todos.service;
 import com.luv2code.springboot.todos.entity.Authority;
 import com.luv2code.springboot.todos.entity.User;
 import com.luv2code.springboot.todos.repository.UserRepository;
+import com.luv2code.springboot.todos.request.PasswordUpdateRequest;
 import com.luv2code.springboot.todos.response.UserResponse;
+import com.luv2code.springboot.todos.util.FindAuthenticatedUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,20 +20,19 @@ import java.util.Objects;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final FindAuthenticatedUser findAuthenticatedUser;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, FindAuthenticatedUser findAuthenticatedUser, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.findAuthenticatedUser = findAuthenticatedUser;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     @Transactional(readOnly = true)
     public UserResponse getUserInfo() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated() ||
-                Objects.equals(authentication.getPrincipal(), "anonymousUser")) {
-            throw new AccessDeniedException("Authentication required");
-        }
-        User user = (User) authentication.getPrincipal();
+        User user = findAuthenticatedUser.getAuthenticatedUser();
         return new UserResponse(
                 user.getId(),
                 user.getFirstName() + " " + user.getLastName(),
@@ -41,18 +43,49 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated() ||
-                Objects.equals(authentication.getPrincipal(), "anonymousUser")) {
-            throw new AccessDeniedException("Authentication required");
-        }
-        User user = (User) authentication.getPrincipal();
+        User user = findAuthenticatedUser.getAuthenticatedUser();
         // isLastAdmin
         if (isLastAdmin(user)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Admin cannot delete itself");
         }
         userRepository.delete(user);
     }
+
+    @Override
+    @Transactional
+    public void updatePassword(PasswordUpdateRequest passwordUpdateRequest) {
+        User user = findAuthenticatedUser.getAuthenticatedUser();
+        if (!isOldPasswordCorrect(user.getPassword(),
+                passwordUpdateRequest.getOldPassword())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Current password is incorrect");
+        }
+        if (!isNewPasswordConfirmed(passwordUpdateRequest.getNewPassword(),
+                passwordUpdateRequest.getNewPassword2())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "New password do not match");
+        }
+        if (!isNewPasswordDifferent(passwordUpdateRequest.getOldPassword(),
+                passwordUpdateRequest.getNewPassword())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Old and new password must be different");
+        }
+        user.setPassword(passwordEncoder.encode(passwordUpdateRequest.getNewPassword()));
+        userRepository.save(user);
+    }
+
+    private boolean isOldPasswordCorrect(String currentPassword, String oldPassword) {
+        return passwordEncoder.matches(oldPassword, currentPassword);
+    }
+
+    private boolean isNewPasswordConfirmed(String newPassword, String newPasswordConfirmed) {
+        return newPassword.equals(newPasswordConfirmed);
+    }
+
+    private boolean isNewPasswordDifferent(String oldPassword, String newPassword) {
+        return !oldPassword.equals(newPassword);
+    }
+
     private boolean isLastAdmin(User user) {
         boolean isAdmin = user.getAuthorities().stream()
                 .anyMatch(authority -> "ROLE_ADMIN".equals(authority.getAuthority()));
